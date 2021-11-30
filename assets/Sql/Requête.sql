a22cea655e5453c892bf0fb8530e455a68425072
WessimH <89280997+WessimH@users.noreply.github.com>
2021-11-30 19:47:33 +0100

    fff

--- assets/Sql/Requête.sql
+++ assets/Sql/Requête.sql
@@ -1,15 +1,2 @@
---CREATE TABLE contact(namecontact VARCHAR(50), first_name  VARCHAR(50))
-
-SELECT   COUNT(*) AS nbr_doublon, first_name, name,
-FROM     table
-GROUP BY first_name, name
-HAVING   COUNT(*) > 1
-
-DELETE FROM TABLE
-LEFT OUTER JOIN (
-        SELECT MIN(id) as id, first_name, name
-        FROM table
-        GROUP BY first_name,name
-    ) as t1 
-    ON table.id = t1.id
-WHERE t1.id IS NULL
+CREATE TABLE contact(namecontact VARCHAR(50), first_name  VARCHAR(50))
+contactcontact
