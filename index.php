6a2bbc5aacd4da982ec7d8dc92ae819ac4e3dc7c
WessimH <slim.harmel@free.fr>
2021-12-02 16:23:24 +0100

    le syle ne s'aplique pas

--- index.php
+++ index.php
@@ -1,22 +1,17 @@
 <?php include("assets/PHP/template/header.php");?>
     <div class="login">
         <div class="title" >
-
+            <h2>test </h2>
         </div>
         <form action="assets\PHP\Admin\index.php" method="post">
-            <div class="form_style"><br>
-                <input type="text" name="nom" maxlength="30
-                "    minlenth ="3"><br>
+                <input type="text" name="nom" maxlength="30"    minlenth ="3"><br>
                 <input type="text" name="prenom" ><br>
                 <input type="date" name="birthday" max="<?php echo date('Ymd')?>">
-                <input type="passworld">
-                    <input type="submit" class="button">
-                </div>                                
-            </div>
+                <input type="submit" class="button">
+
 
         </form>
         </div>
-        <?php include("assets/JavaScript/maps.js")?>
         
 
         </form>         
