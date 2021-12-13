e3525919bb17409153604c8f841fddbe491d203e
WessimH <slim.harmel@free.fr>
2021-12-13 17:05:00 +0100

    crvo kc

--- test/graph2.php
+++ test/graph2.php
@@ -1,55 +1,74 @@
 <html>
+
 <head>
 </head>
 
 <body>
 
-<h3>graph</h3>
-<?php
-function age($birthday) {
+  <h3>graph</h3>
+  <?php
+  function age($birthday)
+  {
     $date = date("ymd"); //ordre de la date year month day
     $age = date('Y', strtotime($date)) - (date('Y', strtotime($birthday))); //calule la différence entre la date de l'utulisateur et celle d'ajd en la convertissant au meme format
     if (strtotime(date('md')) < date('md', strtotime($date))) { /*si  le mois et le jours d'aujourd'hui dépasse la  date d'ajd alors age =age-1 */
-        $age = $age - 1;
+      $age = $age - 1;
     }
     return $age;
-}
-?>
+  }
+  ?>
 
-<canvas id="myChart" width="300" height="300"></canvas>
-<script src="..\assets\JavaScript\chart.js"></script>
+  <canvas id="myChart" width="300" height="300"></canvas>
+  <script src="..\assets\JavaScript\chart.js"></script>
 
-<?php
-echo "<script>
+  <?php
+  include("..\assets\PHP\Admin\connexion.php"); #on inclu la connexion
+
+  $sql_select = 'SELECT birthday FROM contact';
+  $req = $pdo->query($sql_select);
+
+  $age_db = []; // Tableau des différents Age rencontrés
+  while ($row = $req->fetch()) {
+    $date = $row['birthday'];
+    $age_db[] = age($date); // Cette ligne équivaut à array_push($age_db, age($date));  
+  }
+  $min = min($age_db);
+  $max = max($age_db);
+  $tab_repartion_age = array_count_values($age_db);  // tableau de la répartition des âges
+
+
+  echo "<script>
 const ctx = document.getElementById('myChart');
 const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ";
+  //exemple : ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  $str = "[";
+  foreach ($tab_repartion_age as $key => $value) {
+    $str = $str . $key . ",";
+  }
+  $str = substr($str, 0, strlen($str) - 1) . "],";
+  echo $str;
+
+
+  echo "
         datasets: [{
-            label: '# of Votes', ";
+            label: '# of Votes', 
+            ";
 
-//            data: [12, 19, 3, 5, 2, 3],
-    
-    include("..\assets\PHP\Admin\connexion.php"); #on inclu la connexion
+  //exemple :        data: [12, 19, 3, 5, 2, 3],
 
-    $sql_select = 'SELECT birthday FROM contact';
-    $req = $pdo->query($sql_select);
 
-    $age_db = [];
-    while ($row = $req->fetch())
-    {
-        $date = $row['birthday']; 
-        $age = age($date); 
-        
-        array_push($age_db, age($date));
 
-    }
-    var_dump($age_db);
-    //   data: [12, 19, 3, 5, 2, 3,
-    echo $str;
-
-    echo"
+  // }
+  $str = "data: [";
+  foreach ($tab_repartion_age as $key => $value) {
+    $str = $str . $value . ", ";
+  }
+  $str = substr($str, 0, strlen($str) - 1) . "],";
+  echo $str;
+  echo "
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -90,11 +109,12 @@ const myChart = new Chart(ctx, {
         }
       },
 });";
-?>
+  ?>
 
 
-</script>
+  </script>
 
 
 </body>
+
 </html>
